df27d55072da383a75fa23946be1c2f80ccd3cc0
ksavage97 <ksavage97@gmail.com>
2016-08-24 15:36:33 -0700

    first commit

---
+++ TopSpotsAPI/TopSpotsAPI/Controllers/TopSpotsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TopSpotsAPI.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TopSpotsAPI.Controllers
+{
+    public class TopSpotsController : ApiController
+    {
+        //GET /api/topspots
+        public IEnumerable<TopSpots> Get()
+        {
+           TopSpots[] TopSpot = JsonConvert.DeserializeObject<TopSpots[]>(File.ReadAllText(@"C:\Dev\13-TopSpotsAPI\topspots.json"));
+            return TopSpot;
+        }
+    }
+}
